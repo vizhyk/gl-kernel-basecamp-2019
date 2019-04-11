d1f7f0a5a6fd2ba0036f98e9756c09a4fe0b13f3
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-04-11 16:54:44 +0300

    Lesson 5-7 part 2: Add script variables Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- script.sh
+++ script.sh
@@ -1,4 +1,14 @@
 !#/bin/bash
+home_path=/home/vvi/Ker_Modules/chtest/
+proc_file_path=/proc/chtest
+dev_file_path=/dev/chtest
+sys_file_path=/sys/kernel/chtestflag/clrflag
+test_file_path=/home/vvi/Ker_Modules/chtest/
+mod_name=chtest.ko
+test_name=testread_dev
+
+
+
 
 
 
