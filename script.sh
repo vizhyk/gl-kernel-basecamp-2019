9546c27d05b9d512e0fbed9ef3aa9ea91cb636c0
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-04-11 16:56:28 +0300

    Lesson 5-7 part 2: Add commands Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- script.sh
+++ script.sh
@@ -7,6 +7,28 @@ test_file_path=/home/vvi/Ker_Modules/chtest/
 mod_name=chtest.ko
 test_name=testread_dev
 
+echo "Loading modul"
+insmod $home_path$mod_name 
+echo "checking module status"
+cat $proc_file_path
+echo "writing to device"
+echo  "0123456789" > $dev_file_path
+echo "checking module status"
+cat $proc_file_path
+echo "clearing a buffer"
+echo 1 >$sys_file_path
+echo "checking module status"
+cat $proc_file_path
+echo "writing to device\n"
+echo  "0123456789" > $dev_file_path
+echo "checking module status"
+cat $proc_file_path
+echo "reading data from device"
+$test_file_path$test_name
+echo "unlodading buffer"
+rmmod $mod_name
+
+
 
 
 
